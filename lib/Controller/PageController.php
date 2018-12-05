23396f3935e2f0184620d1be0126809d6713a68a
Tim Christensen <tim.christensen@robinwood.de>
2018-12-05 21:58:12 +0100

    order

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -5,10 +5,10 @@ use OCP\IRequest;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
+use OCA\GroupFolders\Folder;
 
 class PageController extends Controller {
 	private $userId;
-
 	public function __construct($AppName, IRequest $request, $UserId){
 		parent::__construct($AppName, $request);
 		$this->userId = $UserId;
--- templates/index.php
+++ templates/index.php
@@ -5,41 +5,6 @@ style('robinwooddashboard', 'style');
 ?>
 
 <div id="app">
-	<div id="app-navigation">
-		<div id="app-settings-content">
-			<p>Dieses Werkzeug wandelt Bilder in schwarz-grüne Bilder für ROBIN WOOD-Veröffentlichungen um<span class="fontpreload">.</span></p>
-			<label>
-				<span>Bild-Datei</span>
-				<input type="file" name="file" id="file" accept="image/*" capture>
-			</label>
-			<label>
-				<span>Ausschnitt</span>
-				<input type="range" min="0" max="1" value="0.5" step="0.01" class="slider" id="ausschnitt">
-			</label>
-			<label>
-				<span>Bildquelle</span>
-				<input type="text" id="quelle" />
-			</label>
-			<strong>Slogan</strong>
-			<label>
-				<span>Slogan</span>
-				<input type="text" id="slogan" />
-			</label>
-			<strong>Zitatbox</strong>
-			<label>
-				<span>Zitat</span>
-				<input type="text" id="zitat" />
-			</label>
-			<label>
-				<span>Zitatgeber*in</span>
-				<input type="text" id="zitatgeberin" />
-			</label>
-			<label>
-				<span>Zitatgeber*in Funktion</span>
-				<input type="text" id="zitatfunktion" />
-			</label>
-		</div>
-	</div>
 	<div id="app-content">
 		<div id="app-content-images">
 			<div class="section" id="overviewPart"></div>
